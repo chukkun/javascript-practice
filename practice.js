26ab823c80bac75bec2e13b3e70dbe966313f3e3
chukkun <sudhanramngl@gmail.com>
2024-09-10 16:24:58 +0530

    function programs

---
+++ practice.js
@@ -0,0 +1,61 @@
+// greet();
+// function greet() {
+//     console.log("Hello, World!");
+// }
+//  greet();
+
+// getRandomNumber();
+// function getRandomNumber() {
+//     return(Math.random());
+// }
+// let num=getRandomNumber();
+// console.log(num);
+
+
+function displayGreeting(nam) {
+    console.log(` Hello, ${nam}! `)
+}
+displayGreeting("vajesh");
+displayGreeting("Subbu");
+
+
+// let a=3;
+// function calculateSquare(a) {
+//     return(a**2);
+// }
+// console.log(calculateSquare(a));
+
+
+// let x="vajesh";
+// let y="babu"
+// function concatString(x,y) {
+//     return(x+y);
+// }
+// let z=concatString(x,y);
+// console.log(z);
+
+
+// let age=20;
+// //  isAdult(2);
+// function isAdult(age){
+//     return(age>=18);
+// }
+// console.log(isAdult(age));
+
+
+// let a=15;
+// let b=15;
+// function addNumbers(a,b) {
+//     return(a+b);
+// }
+// console.log(addNumbers(a,b));
+
+// function isEven(a)  {
+//     if (a%2==0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// console.log(isEven(2));
