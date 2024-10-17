b7ef6c5e742a44fef3b85dbd5679edd8e46c810d
chukkun <sudhanramngl@gmail.com>
2024-10-17 09:52:17 +0530

    js practice

---
+++ pet_practice.js/prac1.js
@@ -0,0 +1,108 @@
+// function num(n){
+//     if(n==2){
+//         return true;
+//     }
+//     return false;
+// }
+// console.log(num(4));
+
+
+
+// function num(n){
+//     a=n.toLowerCase();
+//         if(a=="a"){
+//             return true;
+//         }
+//         return false;
+//     }
+//     console.log(num("A"));
+
+
+// function num(n){
+//    n=n.toLowerCase();
+//       if(n=="a"){
+//          return true;
+//     }
+//     else if(n=="d"||n=="e"||n=="f"){
+//         return false;
+//     }
+//          return 1;
+// }
+// console.log(num("niv"));
+
+// let a=[1,2,3,4,5,6,7];
+// function array(a){
+// sum=0;
+// for(i=0;i<=a.length;i++){
+//     sum+=a[i];
+
+//     if(sum%3===0){
+//         return true;
+//     }
+//     return false;
+// }
+// }
+// console.log(array(a));
+
+
+// let a=[1,2,3,4,5,6,7];
+// function array(a){
+// sum=0;
+// for(i=0;i<=a.length;i++){
+//     sum+=a[i];
+
+//     if(sum%3===0){
+//         return true;
+//     }
+//     return false;    
+// }
+// }
+// console.log(array(a));
+
+// let expense=[1,2,3,4,5,6,7];
+// function avg(expense){
+
+//     let sum=0;
+//     for(i=0;i<expense.length;i++){
+//         sum+=expense[i];
+       
+//     }
+//     console.log(sum);
+//      return `${sum/expense.length}`;
+
+
+// }console.log(avg(expense));
+
+// let array=[1,2,3,4,5];
+// console.log(array[3]);
+
+
+
+// let exp=[1,2,3,4,5,6,7];
+// let increment=15;
+// dailyexpense(exp,increment);
+// function dailyexpense(exp,increment){
+//     let sum=100;
+//     for(let i=0;i<exp.length;i++){
+//         sum+=exp[i]+increment;
+//         console.log(sum);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
