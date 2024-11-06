ee797d51b778af287515b353204d042b46409f8d
chukkun <sudhanramngl@gmail.com>
2024-11-06 13:54:57 +0530

    lv 5 prac

---
+++ L4 prac/prac5.js
@@ -0,0 +1,23 @@
+// Question: Write a function that checks if a given string is a palindrome (reads the same forwards and backwards),
+//  ignoring spaces, punctuation, and case.
+
+
+// function isPalindrome(str) {
+//     const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+//     return cleaned === cleaned.split('').reverse().join('');
+// }
+
+// console.log(isPalindrome("A man, a plan, a canal, Panama"));  
+
+// __________________________________________________________________
+
+
+//  Write a function that reverses the order of words in a given string.
+
+// function reverseWords(str) {
+//     return str.split(' ').reverse().join(' ');
+// }
+
+// console.log(reverseWords("Hello World!"));
+
+// ____________________________________________________________
