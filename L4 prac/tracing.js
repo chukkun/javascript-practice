5a444462e2704df91a003dc779ef75c8351aa5c4
chukkun <sudhanramngl@gmail.com>
2024-11-14 14:05:49 +0530

    l4 question

--- L4 prac/tracing.js
+++ L4 prac/tracing.js
@@ -1,34 +1,99 @@
+// function printStar(n) {
+//     for(let i = 1; i <= n; i++) {
+//         let output = getSpaces(n - i);
+//         let pattern = getNumSequence(i);
+//         console.log(output + pattern);
+
+//     }
+
+//     // Reverse print
+//     for(let i = n - 1; i >=1; i--) {
+//         let output = getSpaces(n - i);
+//         let pattern = getNumSequence(i);
+//         console.log(output + pattern);
+//     }
+
+// }
+
+// function getSpaces(n) {
+//     let outputStr = "";
+//     for(let i = 1; i <= n; i++) {
+//         outputStr += " ";
+//     }
+//     return outputStr;
+// }
+
+// function getNumSequence(n) {
+//     let outputStr = "";
+//     for(let i = 1; i <= n; i++) {
+//         outputStr += i + " ";
+//     }
+//     return outputStr;
+// }
+
+// printStar(3);
+
+
+
+
 function printStar(n) {
-    for(let i = 1; i <= n; i++) {
-        let output = getSpaces(n - i);
+    for(let i=1;i<=n;i++){
+        let output = getSpaces(n-i);
         let pattern = getNumSequence(i);
-        console.log(output + pattern);
-
+        console.log(output+pattern);
     }
 
-    // Reverse print
-    for(let i = n - 1; i >=1; i--) {
-        let output = getSpaces(n - i);
+    for(let i=n-1;i>=1;i--){
+        let output=getSpaces(n-i);
         let pattern = getNumSequence(i);
-        console.log(output + pattern);
+        console.log(output+pattern);
     }
+}
+function getSpaces(n){
+    let outputStr= "";
+    for(let i=1;i<=n;i++){
+        outputStr+= " ";
 
+    }return outputStr;
 }
 
-function getSpaces(n) {
-    let outputStr = "";
-    for(let i = 1; i <= n; i++) {
-        outputStr += " ";
-    }
-    return outputStr;
-}
-
-function getNumSequence(n) {
-    let outputStr = "";
-    for(let i = 1; i <= n; i++) {
-        outputStr += i + " ";
-    }o
-    return outputStr;
+function getNumSequence(n){
+    let outputStr= "";
+    for(let i=1; i<=n;i++){
+        outputStr+=i+ " ";
+    }return outputStr;
 }
 
 printStar(3);
+
+// function printStar(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let output = getSpaces(n - i);
+//         let pattern = getNumSequence(i);
+//         console.log(output + pattern);
+//     }
+
+//     for (let i = n - 1; i >= 1; i--) {
+//         let output = getSpaces(n - i);
+//         let pattern = getNumSequence(i);
+//         console.log(output + pattern);
+//     }
+// }
+
+// function getSpaces(n) {
+//     let outputStr = "";
+//     for (let i = 1; i <= n; i++) {
+//         outputStr += " ";
+//     }
+//     return outputStr;
+// }
+
+// function getNumSequence(n) {
+//     let outputStr = "";
+//     for (let i = 1; i <= n; i++) {  // Corrected the loop to start from 1 and go up to n
+//         outputStr += i + " ";
+//     }
+//     return outputStr.trim();  // Remove trailing space
+// }
+
+// printStar(3);
